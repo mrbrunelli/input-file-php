ebf834dfd925aa530e944988ecdbe69f3174d62e
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-04-02 10:51:04 -0300

    Alterado para fazer upload de várias imagens. Caso queira fazer upload de outros arquivos, volte 1 commit

--- ex1.php
+++ ex1.php
@@ -12,19 +12,19 @@ $arquivo = time();
 
 $tipo = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
 
-$arquivo = $arquivo . "." . $tipo;
+//$arquivo = $arquivo . "." . $tipo;
 
 if ($_FILES['arquivo']['type'] != 'image/jpeg') {
     echo '<script>alert("Não é um arquivo JPG válido");history.back();</script>';
     exit;
-} else if (!move_uploaded_file($_FILES['arquivo']['tmp_name'], 'arquivos/' . $arquivo)) {
+} else if (!move_uploaded_file($_FILES['arquivo']['tmp_name'], 'arquivos/' . $_FILES['arquivo']['name'])) {
     echo '<script>alert("Erro, não foi possível copiar!");history.back();</script>';
     exit;
 }
 
 include "imagem.php";
 
-LoadImg("arquivos/" . $arquivo, $arquivo, "arquivos/");
+LoadImg("arquivos/" . $_FILES['arquivo']['name'], $arquivo, "arquivos/");
 
 echo '<script>alert("Arquivo enviado!");history.back();</script>';
 exit;
